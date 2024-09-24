708ebf59383b700529fbdd1a17981a9b09339307
Ruben Macedo <rubenmacedo@rubens-mbp.lan>
2024-09-24 02:33:00 +0100

    added consts

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -1,6 +1,5 @@
-export const mainnet_rpc = "https://rpc.mainnet.lukso.network";
-export const testnet_rpc = "https://rpc.testnet.lukso.network";
+export const mainnet_rpc = "https://mainnet.base.org";
+export const testnet_rpc = "hhttps://sepolia.base.org";
 
-export const contract_address = "0x66b738baea30243bfe4c168a11e79dd1f930e59c";
-export const chill_address = "0x5B8B0E44D4719F8A328470DcCD3746BFc73d6B14";
-export const admin_address = "0x0ddd3E1E9F4E1D6c50A22A276321186db39e6635";
+export const contract_address = "";
+export const admin_address = "";
